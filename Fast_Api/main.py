ba9eb8c23d98883da2a0675e50b93c59ab8a03d9
Muhammad Bahawal <167578686+MuhammadBahawal@users.noreply.github.com>
2025-01-24 00:06:34 +0500

    updated

--- Fast_Api/main.py
+++ Fast_Api/main.py
@@ -1,6 +1,26 @@
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
+
 app = FastAPI()
+
+
+@app.post("/tutor")
+def tutor():
+    return{
+    "name":"Ahmad",
+    "RollNo":239
+    }
+
+
+@app.post("/tutor/{id}")
+def root_data(id):
+    return{
+        "Rollno":id
+    }
+
+
 
 @app.get("/student")
 def root():
@@ -19,8 +39,7 @@ def university():
 
 
 
-
-    # path parametr
+# path parametr
 
 @app.post("/user{id}")
 def root_app(id):
@@ -28,6 +47,64 @@ def root_app(id):
     "name":"Bahawal",
     "roll":id
     }
+
+
+# qurery parameter
+
+@app.get("/hello/{id}")
+def hello(id,Name:str,depart:str,rollno:int):
+    return{
+        "id":id,
+        "name":Name,
+        "Department":depart,
+        "RollNo":rollno
+    }
+
+
+@app.post("/admin")
+def postadmin(userName:str,RollNo:str):
+    try:
+        return{
+            "status": "success",
+            "data":{
+            "username":userName,
+            "RollNo":RollNo
+            }
+    }
+    except Exception as e:
+        return {
+            "message":str(e),
+            "status":"erroe",
+            "data":None
+        }
+
+
+# request body
+
+class item (BaseModel):
+    id:int
+    name:str
+    Roll :int
+@app.post("/item1")
+def itme(items:item):
+    return items
+
+
+class student(BaseModel):
+    id:int
+    Name:str
+    RollNo:int
+@app.post("/student1")
+def student1(student:student):
+    return student
+
+# for get
+@app.post("/")
+def getStudent(student:student):
+    try:return student
+    except Exception as e:
+        return {"message":str(e)}
+
 
 
 
