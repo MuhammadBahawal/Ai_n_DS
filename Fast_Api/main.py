db4d913bd6c81108a0b9707bdbf3c82c182109dc
Muhammad Bahawal <167578686+MuhammadBahawal@users.noreply.github.com>
2025-01-23 16:51:23 +0500

    path parameter

--- Fast_Api/main.py
+++ Fast_Api/main.py
@@ -2,8 +2,32 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/student")
 def root():
     return{
-        "message":"hello"
+        "message":"hello",
+        "name":"Ahmad",
     }
+@app.post("/")
+def university():
+    return{
+        "name":"Ali",
+        "class":"BSSE",
+        "RollNo":"FSD-FL-239"
+    }
+
+
+
+
+
+    # path parametr
+
+@app.post("/user{id}")
+def root_app(id):
+    return {
+    "name":"Bahawal",
+    "roll":id
+    }
+
+
+
